90d0ffdc98c822e79652e60aca2f6f98ba26c28d
mrl00 <m2l_0@outlook.com>
2024-06-22 14:49:37 -0300

    refactor: create email model
    interface use

--- src/models/email.go
+++ src/models/email.go
@@ -6,37 +6,41 @@ import (
 	"gorm.io/gorm"
 )
 
+type EmailModel struct {
+	Db *gorm.DB
+}
+
 type Email struct {
 	EmailId uint   `gorm:"primaryKey;autoIncrement:true"`
 	UserId  uint   `gorm:"primaryKey;autoIncrement:false"`
 	Email   string `gorm:"unique;not null"`
 }
 
-func CreateEmail(db *gorm.DB, email *Email) {
-	result := db.Select("UserId", "Email").Create(email)
+func (e *EmailModel) CreateEmail(email *Email) {
+	result := e.Db.Select("UserId", "Email").Create(email)
 	if result.Error != nil {
 		log.Fatalf("Failed to insert email into db: %v", result.Error)
 	}
 }
 
-func FindEmailById(db *gorm.DB, email *Email) {
-	result := db.Find(&email)
+func (e *EmailModel) FindEmailById(email *Email) {
+	result := e.Db.Find(&email)
 	if result.Error != nil {
 		log.Fatalf("Cannot find emails with id (%v, %v). Error: %v", email.EmailId, email.UserId, result.Error)
 	}
 }
 
-func FindAllEmails(db *gorm.DB) ([]Email, error) {
+func (e *EmailModel) FindAllEmails() ([]Email, error) {
 	var email []Email
-	result := db.Find(&email)
+	result := e.Db.Find(&email)
 	if result.Error != nil {
 		log.Fatalf("Cannot find emails. Error: %v", result.Error)
 	}
 	return email, nil
 }
 
-func DeleteEmail(db *gorm.DB, email *Email) {
-	result := db.Delete(email)
+func (e *EmailModel) DeleteEmail(email *Email) {
+	result := e.Db.Delete(email)
 	if result.Error != nil {
 		log.Fatalf("Cannot delete email with id (%v, %v). Error: %v", email.EmailId, email.UserId, result.Error)
 	}
