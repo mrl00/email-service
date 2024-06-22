a2bc8b3864e209e5a07a4225c961780bf8cc024b
mrl00 <m2l_0@outlook.com>
2024-06-22 14:21:51 -0300

    feat: create health check endpoint

---
+++ src/web/health_check.go
@@ -0,0 +1,12 @@
+package web
+
+import "net/http"
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
